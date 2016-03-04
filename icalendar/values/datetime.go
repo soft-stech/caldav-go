0bc55255ee56cc1a52a635b4a2dbf692ae57e4de
Sunheng Taing <sunhengtaing@gmail.com>
2016-03-04 12:12:18 -0500

    Removed unused resources

--- icalendar/values/datetime.go
+++ icalendar/values/datetime.go
@@ -150,22 +150,6 @@ func (d *DateTime) DecodeICalParams(params properties.Params) error {
 	}
 }
 
-//func getOlsonTimezone(windowsTime string) string {
-//	_, realFilename, _, _ := runtime.Caller(1)
-//	absPath := path.Join(path.Dir(realFilename), "../../timezonesmap/windows-olson.json")
-//	j, err := ioutil.ReadFile(absPath)
-//	if err != nil {
-//		return ""
-//	}
-//	var timeMap map[string]string
-//	if json.Unmarshal(j, &timeMap) != nil {
-//		return ""
-//	}
-//
-//	return timeMap[windowsTime]
-//
-//}
-
 // validates the datetime value against the iCalendar specification
 func (d *DateTime) ValidateICalValue() error {
 
