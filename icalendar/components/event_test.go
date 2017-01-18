3367038dd61146a9793157fd2d6a6d2a97b43f5f
Honza Křeček <killerfrca@gmail.com>
2017-01-18 13:03:19 +0100

    Fix Event Test
    Signed-off-by: Honza Křeček <killerfrca@gmail.com>

--- icalendar/components/event_test.go
+++ icalendar/components/event_test.go
@@ -144,7 +144,7 @@ END:VEVENT`
 	e := Event{}
 	err := icalendar.Unmarshal(raw, &e)
 	c.Assert(err, IsNil)
-	c.Assert(len(e.Attendees), Equals, 1)
+	c.Assert(len(e.Attendees), Equals, 2)
 	c.Assert(e.Attendees[0].Entry.Address, Equals, "fakemcfakebiz.com_b3a0grbjdr4dcje2fc4ikmaeq8@group.calendar.google.com")
 	c.Assert(e.Attendees[0].Entry.Name, Equals, "Fakebiz Shared")
 }
